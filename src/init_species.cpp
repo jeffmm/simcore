e83ab85d63a68bb8c61fa9860433f3a89a43f983
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2016-05-18 19:28:30 -0600

    renamed species to composite, made a species container
    Former-commit-id: e3a5e5a443b31d0c62aca8c7a5f81edb14c4f471

--- src/init_species.cpp
+++ src/init_species.cpp
@@ -1,7 +1,5 @@
 #ifdef _CYTOSCORE_BROWNIAN_DIMER_H_
-for (int i=0; i<params_.n_dimer; ++i) {
-  SpeciesBase * bdmr = new BrownianDimer(params_.n_dim, params_.delta, gsl_rng_get(rng.r));
-  bdmr->Init(&params_, space.GetStruct());
-  species_.push_back(bdmr);
-}
+SpeciesBase * spcs = new Species<BrownianDimer>(params_.n_dimer, &params_, space_.GetStruct(), gsl_rng_get(rng_.r));
+spcs->Init();
+species_.push_back(spcs);
 #endif
