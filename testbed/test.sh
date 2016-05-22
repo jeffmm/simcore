946cad569b12f1359ec09a10eec38aa348e6d3a3
CHRISTOPHER EDELMAIER <christopher.edelmaier@gmail.com>
2016-05-22 12:15:30 -0600

    Finally convinced that this is working
    Former-commit-id: a98e8011419afc6c4cf1acb0e68890758e8ec5d2

--- testbed/test.sh
+++ testbed/test.sh
@@ -4,8 +4,8 @@ make remove
 make CFG=test
 
 echo "Running test on Argon 108 atoms"
-echo "Static test first"
 echo ""
+echo "Static tests:"
 
 echo "Brute Force Scheme (static)"
 ./xtime.rb bin/ArNe_sim argon_108_static.inp brute &> tests/static_brute_test.txt
@@ -18,7 +18,13 @@ echo "Neighbor List All Pairs Scheme (static)"
 
 
 echo ""
-echo "Dynamic test"
+echo "Dynamic tests:"
 
 echo "Brute Force Scheme (dynamic)"
 ./xtime.rb bin/ArNe_sim argon_108_dynamic.inp brute &> tests/dynamic_brute_test.txt
+
+echo "Cell List Scheme (dynamic)"
+./xtime.rb bin/ArNe_sim argon_108_dynamic.inp cells &> tests/dynamic_cell_test.txt
+
+echo "Neighbor List All Pairs Scheme (dynamic)"
+./xtime.rb bin/ArNe_sim argon_108_dynamic.inp allpairs &> tests/dynamic_neighbor_allpairs_test.txt
