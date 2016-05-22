c6174cd22100c446720b9c4d82aa59be5c2c5240
CHRISTOPHER EDELMAIER <christopher.edelmaier@gmail.com>
2016-05-22 12:11:32 -0600

    Dynamic argon nao
    Former-commit-id: e64fdd9e9e854ca11e792fdbc44ed8a2d337ee4d

--- testbed/test.sh
+++ testbed/test.sh
@@ -7,14 +7,18 @@ echo "Running test on Argon 108 atoms"
 echo "Static test first"
 echo ""
 
-echo "Brute Force Scheme"
+echo "Brute Force Scheme (static)"
 ./xtime.rb bin/ArNe_sim argon_108_static.inp brute &> tests/static_brute_test.txt
 
-echo "Cell List Scheme"
+echo "Cell List Scheme (static)"
 ./xtime.rb bin/ArNe_sim argon_108_static.inp cells &> tests/static_cell_test.txt
 
-echo "Neighbor List All Pairs Scheme"
+echo "Neighbor List All Pairs Scheme (static)"
 ./xtime.rb bin/ArNe_sim argon_108_static.inp allpairs &> tests/static_neighbor_allpairs_test.txt
 
-#echo "Neighbor List Cell Scheme"
-#./xtime.rb bin/ArNe_sim argon_108.inp neighborcells &> tests/static_neighbor_cell_test.txt
+
+echo ""
+echo "Dynamic test"
+
+echo "Brute Force Scheme (dynamic)"
+./xtime.rb bin/ArNe_sim argon_108_dynamic.inp brute &> tests/dynamic_brute_test.txt
