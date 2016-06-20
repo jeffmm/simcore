a273c90df7df4710b54991999ecdd1ec2f2b2f99
Jeffrey Moore <moore.jeffrey.m@gmail.com>
2016-06-20 16:08:52 -0600

    finish removing testbed
    Former-commit-id: ea8aa1b30cf2e3ee143bc1bd8db8f33bbb6f011c

--- testbed/run.sh
+++
@@ -1,8 +0,0 @@
-ftype="$1"
-
-echo "argon_sim 108 atoms"
-./xtime.rb bin/ArNe_sim argon_108.inp $ftype
-echo "argon_sim 2916 atoms"
-./xtime.rb bin/ArNe_sim argon_2916_long.inp $ftype
-echo "argon_sim 78732 atoms"
-./xtime.rb bin/ArNe_sim argon_78732_long.inp $ftype
