ecce53b9093b8a581e6fb8e6db1a0191856c7ba4
CHRISTOPHER EDELMAIER <christopher.edelmaier@gmail.com>
2016-07-13 15:53:16 -0600

    adding some stuff for later
    Former-commit-id: 372c080d51b1229dad805adbc8a9fadfd467c71c

--- src/helpers.h
+++ src/helpers.h
@@ -80,4 +80,23 @@ namespace rfh {
     };
 }
 
+// hash helpers
+namespace hashh {
+  template <class T>
+  inline void hash_combine(std::size_t &seed, const T &v) {
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  }
+
+  struct pair_hash {
+    template<typename S, typename T>
+    inline std::size_t operator()(const std::pair<S, T> &v) const {
+      std::size_t seed = 0;
+      hash_combine(seed, v.first);
+      hash_combine(seed, v.second);
+      return seed;
+    }
+  };
+}
+
 #endif
