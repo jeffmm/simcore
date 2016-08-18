a4301a6faee454c0b4be358a17db15e55a58a0f2
CHRISTOPHER EDELMAIER <christopher.edelmaier@gmail.com>
2016-08-18 15:05:32 -0600

    starting test management stuff
    Former-commit-id: 4cfca7cfe3d486196e445595da1514fad12e9acd [formerly c83d02acfe25be19ea2d553eb05a756414026cfc] [formerly c83d02acfe25be19ea2d553eb05a756414026cfc [formerly 3ae160040d47a9344ce1ac50d4448fdb3ef4cdc6]]
    Former-commit-id: 248e571b5cf9958c0bf2e99f7a0e3b00f464f45e
    Former-commit-id: 0be055a6a7d631be730177c5e75282d1f2ccd6af

--- src/parse_flags.h
+++ src/parse_flags.h
@@ -8,13 +8,14 @@
 //       as well as any flag descriptions in the static string array below
 //       when adding new flags.
 
-static const int n_flags = 5; 
+static const int n_flags = 6; 
 static struct option long_options[] = {
   {"help", no_argument, 0, 'h'},
   {"debug", no_argument, 0, 'd'},
   {"file", required_argument, 0, 'f'},
   {"run-name", required_argument, 0, 'r'},
   {"n-runs", required_argument, 0, 'n'},
+  {"test", no_argument, 0, 't'},
   {0, 0, 0, 0}
 };
 
@@ -24,14 +25,16 @@ static const std::string desc[n_flags][2] = {
   {"run program in debug mode\n", "none"},
   {"where fname is the input parameter file (REQUIRED)\n", "fname"},
   {"where rname is the name of a run session (for organizing batch jobs)\n", "rname"},
-  {"where num is the number of independent runs to perform with the given parameters\n", "num"}
+  {"where num is the number of independent runs to perform with the given parameters\n", "num"},
+  {"run program in test mode\n", "none"}
 };
 
 // Run parameters that needs to be carried forward should be stored here.
 struct run_options {
-  run_options() {n_runs = 1; debug = 0; f_flag = 0; r_flag = 0; n_flag = 0; run_name = "sc";}
+  run_options() {n_runs = 1; debug = 0; test = 0; f_flag = 0; r_flag = 0; n_flag = 0; run_name = "sc";}
   int n_runs;
   int debug;
+  int test;
   int f_flag;
   int r_flag;
   int n_flag;
@@ -76,7 +79,7 @@ run_options parse_opts(int argc, char *argv[]) {
   int tmp;
   while (1) {
     int option_index = 0;
-    tmp = getopt_long(argc, argv, "hdr:f:n:", long_options, &option_index);
+    tmp = getopt_long(argc, argv, "hdtr:f:n:", long_options, &option_index);
     if (tmp == -1)
       break;
     
@@ -89,6 +92,10 @@ run_options parse_opts(int argc, char *argv[]) {
       case 'd':
         std::cout << "  Running with Debug Mode\n";
         run_opts.debug = 1;
+        break;
+      case 't':
+        std::cout << "  Running Test Mode\n";
+        run_opts.test = 1;
         break;
       case 'f':
         run_opts.f_flag = 1;
