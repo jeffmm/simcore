73aeed6cd751d59d80f31b8f7229d10554dd7003
Adam Lamson <adam.lamson@colorado.edu>
2016-10-01 15:14:00 -0600

    Nematic insertion of br_rods now implemented
    Former-commit-id: e1377ef198830b2e7dcc26fbea0aad2a503dd4e5
    Former-commit-id: 83e3e243fe14266ef7de10afc0129d8406320717
    Former-commit-id: e3845355e6a3ff202b6700e1b417000b7dccd9bd

--- src/object.cpp
+++ src/object.cpp
@@ -157,6 +157,53 @@ void Object::InsertRandom(double buffer) {
   UpdatePeriodic();
 }
 
+void Object::InsertOriented( double buffer, const double* const u){
+  SetOrientation(u);
+  double mag;
+  if (space_->n_periodic == n_dim_)
+    buffer = 0;
+  double R = space_->radius;
+  if (R - buffer < 0) 
+    error_exit("ERROR: Object #%d is too large to place in system.\n",GetOID());
+  if (space_->type.compare("sphere")==0) {
+    generate_random_unit_vector(n_dim_, position_, rng_.r);
+    mag = gsl_rng_uniform_pos(rng_.r) * (R - buffer);
+    for (int i=0; i<n_dim_; ++i) {
+      position_[i] *= mag;
+    }
+  }
+  else if (space_->type.compare("cube")==0) {
+    for (int i=0; i<n_dim_; ++i)
+      position_[i] = (2.0*gsl_rng_uniform_pos(rng_.r)-1.0) * (R - buffer);
+  }
+  else if (space_->type.compare("snowman")==0) {
+    double r = space_->bud_radius;
+    double roll = gsl_rng_uniform_pos(rng_.r);
+    double v_ratio;
+    if (n_dim_ == 2)
+      v_ratio = SQR(r) / (SQR(r)+SQR(R));
+    if (n_dim_ == 3)
+      v_ratio = CUBE(r) / (CUBE(r)+CUBE(R));
+    mag = gsl_rng_uniform_pos(rng_.r);
+    generate_random_unit_vector(n_dim_, position_, rng_.r);
+    if (roll < v_ratio) {
+      // Place coordinate in daughter cell
+      mag *= (r - buffer);
+      for (int i=0; i<n_dim_; ++i) {
+        position_[i] *= mag;
+      }
+      position_[n_dim_-1] += space_->bud_height;
+    }
+    else {
+      mag *= (R - buffer);
+      for (int i=0; i<n_dim_; ++i) {
+        position_[i] *= mag;
+      }
+    }
+  }
+  UpdatePeriodic();
+}
+
 void Object::Draw(std::vector<graph_struct*> * graph_array) {
   std::copy(position_, position_+3, g_.r);
   std::copy(orientation_, orientation_+3, g_.u);
