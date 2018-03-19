db9f0fa15f21701f79d256773f4d97490534ab14
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2018-03-19 00:54:47 -0600

    Updated testbed for new cell lists
    Former-commit-id: 75879121a9b740281db4b49a94ecfa57c26cb450

--- testbed/simulation.cpp
+++ testbed/simulation.cpp
@@ -4,6 +4,8 @@ void Simulation::Run() {
   objs.resize(NOBJS);
   InitRNG(); // Init RNGs
   InitPositions(); // Init object positions
+  CreatePairs();
+  //PrintPairs();
 }
 
 // Initialize random number generators
@@ -11,10 +13,10 @@ void Simulation::InitRNG() {
   gsl_rng_env_setup();
 #pragma omp parallel
   {
-    int threadnum = omp_get_thread_num();
-    int numthreads = omp_get_num_threads();
-    int low = NOBJS*threadnum/numthreads;
-    int high = NOBJS*(threadnum+1)/numthreads;
+    int tid = omp_get_thread_num();
+    int n_threads = omp_get_num_threads();
+    int low = NOBJS*tid/n_threads;
+    int high = NOBJS*(tid+1)/n_threads;
     for (int i=low; i<high; ++i) {
       objs[i].rng = gsl_rng_alloc(gsl_rng_taus);
       gsl_rng_set(objs[i].rng, (i+1)*SEED);
@@ -25,17 +27,17 @@ void Simulation::InitRNG() {
 void Simulation::InitPositions() {
 #pragma omp parallel
   {
-    int threadnum = omp_get_thread_num();
-    int numthreads = omp_get_num_threads();
-    int low = NOBJS*threadnum/numthreads;
-    int high = NOBJS*(threadnum+1)/numthreads;
-    for (int k=0; k<100; ++k)
+    int tid = omp_get_thread_num();
+    int n_threads = omp_get_num_threads();
+    int low = NOBJS*tid/n_threads;
+    int high = NOBJS*(tid+1)/n_threads;
+    //for (int k=0; k<100; ++k) // For time testing
     for (int i=low; i<high; ++i) {
       objs[i].SetRandomPosition();;
     }
   }
 }
 
-void Simulation::CreatePairs() {
+//void Simulation::CreatePairs() {
 
-}
+//}
