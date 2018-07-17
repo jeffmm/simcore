fdfa33429182c08adf4f0cedf150a402f8b73249
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2018-07-17 11:53:37 -0600

    No longer require posit files for movies/analysis that use spec files
    Former-commit-id: c97f7ce0df67a6f1e32e5a23546ed31eb01b4c7d

--- src/species.cpp
+++ src/species.cpp
@@ -104,7 +104,7 @@ void SpeciesBase::LoadFromCheckpoints(std::string run_name, std::string checkpoi
 }
 
 void SpeciesBase::InitInputFiles(std::string run_name, bool posits_only) {
-  if (sparams_->posit_flag) 
+  if (posits_only && sparams_->posit_flag) 
     InitPositFileInput(run_name);
   if (!posits_only && sparams_->spec_flag) 
     InitSpecFileInput(run_name);
