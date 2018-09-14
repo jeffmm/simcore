64c32122c6b3b06e84cfb69ad79b0c0f6f3c5f4f
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2018-09-13 18:26:50 -0600

    Adding orientation correlation analysis
    Former-commit-id: 79acbb3aa8421bbd4249cc9a4520f9150ac8e284

--- src/mesh.h
+++ src/mesh.h
@@ -69,7 +69,7 @@ class Mesh : public Object {
     virtual void SetAvgPosition();
     virtual void GetContactNumbers(std::vector<double> * cn);
     virtual void GetPolarOrders(std::vector<double> * po);
-    //std::pair<double,double> Mesh::GetAvgOrientationCorrelation();
+    std::pair<double,double> GetAvgOrientationCorrelation();
 };
 
 #endif // _SIMCORE_MESH_H_
