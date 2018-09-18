bd0c61473273d2ed31ff54ec9446f35afa5cbf2b
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2018-09-18 16:07:28 -0600

    Updated overlap analysis
    Former-commit-id: 4159b740e5fa39e9c6851a91dbce838a89e6827c

--- src/neighbor_list.h
+++ src/neighbor_list.h
@@ -1,16 +1,46 @@
-// All neighbor list implementations and information
-
 #ifndef _SIMCORE_NEIGHBOR_LIST_H_
 #define _SIMCORE_NEIGHBOR_LIST_H_
 
-#include <vector>
+#include <map>
+#include <unordered_set>
 
-struct _neighbor_kmc {
-  int idx_;
-  int g_idx_; // global index of particle
-  double kmc_ = 0.0;
+typedef std::unordered_set<int> registry;
+typedef std::map<int, registry> verlet_list;
+
+class NeighborList {
+  private:
+    registry reg_; // list of object ids in our verlet list
+    verlet_list table_;
+    void Register(int id) {
+      reg_.insert(id);
+      registry neighbors;
+      table_[id] = neighbors;
+    }
+    // returns true if not previously registered (id does not exist in table)
+    bool NotRegistered(int id) {
+      if (reg_.find(id) == reg_.end()) return true;
+      return false;
+    }
+
+  public:
+    //  Returns true if neighbors, false if not neighbors
+    bool AreNeighbors(int id1, int id2) {
+      if (NotRegistered(id1) || NotRegistered(id2)) return false;
+      else if (table_[id1].find(id2) == table_[id1].end()) return false;
+      else return true;
+    }
+    void AddNeighbors(int id1, int id2) {
+      if (NotRegistered(id1)) Register(id1);
+      if (NotRegistered(id2)) Register(id2);
+      table_[id1].insert(id2);
+      table_[id2].insert(id1);
+    }
+    void RemoveNeighbors(int id1, int id2) {
+      table_[id1].erase(id2);
+      table_[id2].erase(id1);
+    }
 };
-typedef struct _neighbor_kmc neighbor_kmc_t;
-typedef std::vector<neighbor_kmc_t> nl_kmc_list;
+
+
 
 #endif
