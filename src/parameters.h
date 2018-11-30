1fe405e30d24cfdf9ec17f5b8e6224f095127ddc
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2018-11-30 15:12:52 -0700

    Added intrinsic curvature option to filaments
    Former-commit-id: 1953120db3ba800193e2693029e6f85b792c259a
    Former-commit-id: e596539f55ec89ea126ebe9f53d836348b51e032
    Former-commit-id: 51d9a665157371b42a29718f9f5ee8e0ad49a450

--- src/parameters.h
+++ src/parameters.h
@@ -58,6 +58,7 @@ class filament_parameters : public species_parameters {
     int orientation_corr_analysis = 0;
     int orientation_corr_n_steps = 1000;
     int crossing_analysis = 0;
+    double intrinsic_curvature = 0;
 };
 
 class passive_filament_parameters : public species_parameters {
