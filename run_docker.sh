80a39d02e15bbf2cc2ef1d01df4507d486e0fd6e
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2019-09-03 17:35:11 -0600

    Updated run_docker script

--- run_docker.sh
+++ run_docker.sh
@@ -6,9 +6,9 @@
 # If 'build' argument was provided, rebuild images
 if [ "$1" == "build" ]; then
   echo "Building images with docker-compose"
-  docker-compose -f docker/docker-compose.yml --project-name mdsim up -d --build
+  docker-compose -f docker/docker-compose.yml --project-name simcore up -d --build
 # Otherwise, just start up the containers
 else
   echo "Running containers with docker-compose"
-  docker-compose -f docker/docker-compose.yml --project-name mdsim up -d
+  docker-compose -f docker/docker-compose.yml --project-name simcore up -d
 fi
