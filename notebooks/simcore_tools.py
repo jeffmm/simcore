3082f257955e7712b5480cae991f325d478ef4e5
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2020-02-21 15:06:42 -0700

    Update simcore_tools

--- notebooks/simcore_tools.py
+++ notebooks/simcore_tools.py
@@ -1,27 +1,36 @@
 """Useful functions for parsing/analyzing simcore output files
 
-Functions include spec_reader and posit_reader for reading simcore .spec and .posit
-files.
+Functions include SpecReader and PositReader for reading simcore .spec and .posit
+files as well as FileRenamer to rename simcore output files en masse corresponding to
+parameters in params.yaml files.
 """
 
 __version__ = "0.1"
 __author__ = "Jeffrey M. Moore"
 
-from struct import unpack, calcsize
+from struct import unpack, pack, calcsize
 from mmap import ACCESS_READ, mmap
 from pathlib import Path
+import re
+import yaml
 
+import numpy as np
 import pandas as pd
 
 
-class spec_reader:
+class SpecReader:
     """Convert simcore .spec files into human readable formats.
 
     Example:
-        sreader = spec_reader("my_spec_file.spec")
+        sreader = SpecReader("my_spec_file.spec")
         sreader.make_human_readable()
         params, specs = sreader.get_params_specs()
 
+    Functions:
+        make_human_readable
+        get_params_specs
+        convert_old_spec
+        rm_human_readable
     """
 
     def __init__(self, spec_file):
@@ -31,13 +40,13 @@ class spec_reader:
             spec_file (string or Path): Location of spec file
 
         """
-        self.input_file = Path(spec_file)
-        if not self.input_file.is_file():
-            raise FileNotFoundError("Could not find file {}", self.input_file)
-        self.output_file = Path(str(spec_file) + ".hr")
-        self.bpos = 0
+        self._input_file = Path(spec_file)
+        if not self._input_file.is_file():
+            raise FileNotFoundError("Could not find file {}", self._input_file)
+        self._output_file = Path(str(spec_file) + ".hr")
+        self._bpos = 0
 
-    def write(self, data_array, fout):
+    def _write(self, data_array, fout):
         """TODO."""
         fout.write(str(data_array[0]))
         if len(data_array) > 1:
@@ -45,25 +54,74 @@ class spec_reader:
                 fout.write(" " + str(datum))
         fout.write("\n")
 
-    def read(self, fmt, buffer):
+    def _read(self, fmt, buffer):
         """TODO"""
-        old_bpos = self.bpos
-        self.bpos += calcsize(fmt)
-        retval = unpack(fmt, buffer[old_bpos : self.bpos])
+        old_bpos = self._bpos
+        self._bpos += calcsize(fmt)
+        retval = unpack(fmt, buffer[old_bpos : self._bpos])
         if len(retval) == 1:
             return retval[0]
         else:
             return retval
 
+    def _writeb(self, data_array, write_as, fout):
+        """Write data_array to output file fout as binary
+
+        Args:
+            data_array (list): list of data to write
+            write_as (str): format to write data e.g. "iid" for int int double
+            fout (file): binary output file
+
+        """
+        if len(write_as) == 1:
+            fout.write(pack(write_as, data_array))
+        else:
+            fout.write(pack(write_as, *data_array))
+
+    def convert_old_spec(self):
+        """Convert old filament spec file from binary to binary with prefix 'new_'"""
+        out_file = Path(self._input_file.parent, "new_" + self._input_file.name)
+        with open(self._input_file, "rb", 0) as f, mmap(
+            f.fileno(), 0, access=ACCESS_READ
+        ) as s, open(out_file, "wb+") as fout:
+            header = self._read("iid", s)
+            self._writeb(header, "iid", fout)
+            while True:
+                try:
+                    num = self._read("i", s)
+                    self._writeb(num, "i", fout)
+                    for i in range(num):
+                        # diameter, length, lp, friction_par, friction_perp, l_bond
+                        diameter, length, lp = self._read("ddd", s)
+                        self._bpos += calcsize("dd")
+                        l_bond, n_bonds = self._read("di", s)
+                        # rtail[3], rhead[3]
+                        r_site = np.array(self._read("ddd", s))
+                        self._bpos += calcsize("ddd")
+                        # u[3] for each bond
+                        self._writeb([diameter, length, l_bond, n_bonds + 1],
+                                     "dddi", fout)
+                        self._writeb(list(r_site), "ddd", fout)
+                        for bond in range(n_bonds):
+                            u_bond = np.array(self._read("ddd", s))
+                            r_site += l_bond * u_bond
+                            self._writeb(list(r_site), "ddd", fout)
+                        self._writeb([lp, 0], "dB", fout)
+
+                except Exception as e:
+                    print("EOF reached:", e)
+                    break
+        self._bpos = 0
+
     def make_human_readable(self):
         """Convert spec file from binary to plaintext with extension '.hr'"""
 
-        with open(self.input_file, "rb", 0) as f, mmap(
+        with open(self._input_file, "rb", 0) as f, mmap(
             f.fileno(), 0, access=ACCESS_READ
-        ) as s, open(self.output_file, "w+") as fout:
+        ) as s, open(self._output_file, "w+") as fout:
 
-            header = self.read("iid", s)
-            self.write(
+            header = self._read("iid", s)
+            self._write(
                 [
                     "n_steps",
                     "n_spec",
@@ -79,28 +137,28 @@ class spec_reader:
             )
 
             n_samples = header[0] // header[1]
-            num = self.read("i", s)
-            details = self.read("ddd", s)
-            n_sites = self.read("i", s)
-            posits = list(self.read("ddd" * n_sites, s))
-            perlen = self.read("d", s)
-            self.bpos += calcsize("c")
-            self.write(list(header) + [num] + list(details) + [perlen, n_sites], fout)
-            self.write(["site_positions_x_y_z"], fout)
+            num = self._read("i", s)
+            details = self._read("ddd", s)
+            n_sites = self._read("i", s)
+            posits = list(self._read("ddd" * n_sites, s))
+            perlen = self._read("d", s)
+            self._bpos += calcsize("c")
+            self._write(list(header) + [num] + list(details) + [perlen, n_sites], fout)
+            self._write(["site_positions_x_y_z"], fout)
             for fil in range(1, num):
-                self.bpos += calcsize("dddi")
-                posits += list(self.read("ddd" * n_sites, s))
-                self.bpos += calcsize("dc")
-            self.write(posits, fout)
+                self._bpos += calcsize("dddi")
+                posits += list(self._read("ddd" * n_sites, s))
+                self._bpos += calcsize("dc")
+            self._write(posits, fout)
             for sample in range(1, n_samples):
-                self.bpos += calcsize("i")
+                self._bpos += calcsize("i")
                 posits = []
                 for fil in range(num):
-                    self.bpos += calcsize("dddi")
-                    posits += list(self.read("ddd" * n_sites, s))
-                    self.bpos += calcsize("dc")
-                self.write(posits, fout)
-        self.bpos = 0
+                    self._bpos += calcsize("dddi")
+                    posits += list(self._read("ddd" * n_sites, s))
+                    self._bpos += calcsize("dc")
+                self._write(posits, fout)
+        self._bpos = 0
 
     def get_params_specs(self):
         """Return spec file parameters and specs timeseries as dataframes
@@ -110,13 +168,13 @@ class spec_reader:
                 and Pandas DataFrame of specs timeseries.
 
         """
-        if not self.output_file.is_file():
+        if not self._output_file.is_file():
             raise FileNotFoundError(
-                "Human readable file not found: {}".format(self.output_file)
+                "Human readable file not found: {}".format(self._output_file)
             )
-        params = pd.read_csv(self.output_file, delim_whitespace=True, nrows=1)
+        params = pd.read_csv(self._output_file, delim_whitespace=True, nrows=1)
         specs = pd.read_csv(
-            self.output_file, skiprows=3, delim_whitespace=True, header=None
+            self._output_file, skiprows=3, delim_whitespace=True, header=None
         )
         n_sites = params.n_sites[0]
         n_fils = params.n_filaments[0]
@@ -144,21 +202,22 @@ class spec_reader:
 
     def rm_human_readable(self):
         """TODO"""
-        if self.output_file.is_file():
-            self.output_file.unlink()
+        if self._output_file.is_file():
+            self._output_file.unlink()
         else:
             raise FileNotFoundError(
-                "Human readable file not found: {}".format(self.output_file)
+                "Human readable file not found: {}".format(self._output_file)
             )
 
 
-class posit_reader:
+class PositReader:
     """Convert simcore .posit files into human readable formats.
 
     Example:
-        preader = posit_reader("my_posit_file.posit")
-        preader.make_human_readable()
-        params, posits = preader.get_params_posits()
+    >>> from simcore_tools import PositReader
+    >>> preader = PositReader("my_posit_file.posit")
+    >>> preader.make_human_readable()
+    >>> params, posits = preader.get_params_posits()
 
     """
 
